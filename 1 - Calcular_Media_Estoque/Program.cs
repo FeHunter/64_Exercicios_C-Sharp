2a5bda17aea4f65c6ea012a61295a73edfded35a
Felipe Rodrigues (Hunter) <41969682+FeHunter@users.noreply.github.com>
2023-06-24 11:22:13 -0300

    Add files via upload
    Programa que calcular a média disponível em um estoque.

---
+++ 1 - Calcular_Media_Estoque/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicios {
+    class Program {
+        static void Main (){
+                        
+            try {
+                Console.WriteLine("Quantidade Maxima:");
+                int _qMaxima = int.Parse(Console.ReadLine());
+                
+                
+                Console.WriteLine("Quantidade Minima");
+                int _qMinima = int.Parse(Console.ReadLine());
+                
+                int _media = (_qMaxima + _qMinima) / 2;
+                
+                Console.WriteLine("A Media do estoque é " + _media);
+            }
+            catch (Exception e){
+                Console.WriteLine($"Erro: {e.Message}");
+            }
+        }
+    }
+}
