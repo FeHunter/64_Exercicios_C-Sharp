77def6dcd4dfe57687bc7074905aff9f6c5f4f08
Felipe Rodrigues (Hunter) <41969682+FeHunter@users.noreply.github.com>
2023-06-24 11:24:23 -0300

    Add files via upload

---
+++ 4 - Ler 4 valores e mostrar resultado/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class HelloWorld{
+  static void Main (){
+      
+      try {
+        int[] numero = new int[4];
+        int[] somas = new int[4];
+        int[] multi = new int[4];
+        char[] letra = new char[4] {'A', 'B', 'C', 'D'};
+
+        for (int i=0; i < 4; i++){
+            Console.Write($"Digite o {i+1}° número para {letra[i]}: ");
+            numero[i] = int.Parse(Console.ReadLine());
+        }
+
+        for (int i=0; i < 4; i++){
+            Console.WriteLine($"\nResultados de ( {letra[i]} ):");
+            for (int j=0; j < 4; j++){
+                somas[i] = ( numero[i] + numero[j] );
+                multi[i] = ( numero[i] * numero[j] );
+                Console.WriteLine($"Soma {letra[i]} + {letra[j]} = {somas[i]}");
+                Console.WriteLine($"Multi {letra[i]} x {letra[j]} = {multi[i]}");
+                Console.WriteLine("........");
+            }
+        }
+      }
+      catch (Exception e){
+        Console.WriteLine($"Error: {e.Message}");
+      }
+  }
+}
+
+
---
+++ 5 - Litros de combustível gastos em uma viagem/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+class Program{
+  static void Main (){
+      
+      try {
+        Console.WriteLine("\n** Calcular litros gastos em uma viagem **\n");
+
+        Console.WriteLine("Qual a duração da viagem?\nDigite no formato horas:minutos. Ex: 01:20 'Uma hora e vine minutos'.");
+        string getTempo = Console.ReadLine();
+        string[] formatarTempo = getTempo.Split(":");
+        TimeSpan tempo = new TimeSpan(int.Parse(formatarTempo[0]), int.Parse(formatarTempo[1]), 0);
+        double tempoCal = tempo.Hours + tempo.Minutes;
+        
+        Console.WriteLine("\nQual a velocidade média em KM/H durante a viagem?");
+        double velocidade = double.Parse(Console.ReadLine());
+        
+        double distancia = tempoCal * velocidade;
+        double litrosUsados = distancia / 12;
+        
+        Console.WriteLine("\nResultados:");
+        Console.WriteLine($"\nVelocidade Média: {velocidade} Km/h \nTempo Média: {tempo} \nDistância Percorrida: {distancia} KM \nLitros Gastos: {litrosUsados.ToString("F2", CultureInfo.InvariantCulture)} L\n");
+      }
+      catch (Exception e){
+        Console.WriteLine($"Error: {e.Message}");
+      }
+  }
+}
+
