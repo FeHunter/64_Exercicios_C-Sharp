b3ea9cc7b7b0b043dbfdba2840c344bcc3f5ac27
Felipe Rodrigues (Hunter) <41969682+FeHunter@users.noreply.github.com>
2023-06-24 17:50:15 -0300

    Add files via upload

---
+++ 10 - Relação Menor, Maior, Igual e Diferente Entre dois números/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+class Program{
+  static void Main (){
+      
+      Console.WriteLine("Digite o 1° número");
+      int n1 = int.Parse(Console.ReadLine());
+      
+      Console.WriteLine("Digite o 2° número");
+      int n2 = int.Parse(Console.ReadLine());
+      
+      
+      // Igual e Não Igual
+      if (n1 == n2){
+          Console.WriteLine($"O N° {n1} é Igual ao N° {n2}");
+      }else {
+          Console.WriteLine($"O N° {n1} é diferente N° {n2}");
+      }
+      
+      // Maior e Menor
+      if (n1 > n2){
+          Console.WriteLine($"O N° {n1} e maior que o N° {n2}");
+      }else {
+          Console.WriteLine($"O N° {n1} e menor que o N° {n2}");
+      }
+      
+       // Maior ou Igual e Menor ou Igual
+      if (n1 >= n2){
+          Console.WriteLine($"O N° {n1} e maior ou igual que o N° {n2}");
+      }else {
+          Console.WriteLine($"O N° {n1} e menor ou igual que o N° {n2}");
+      }
+  }
+}
+
---
+++ 9 - Idade apenas em dia N°/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Programa {
+    class Program{
+        static void Main (){
+        
+            const double ANO = 365;
+            const double MES = 30;
+              
+            Console.Write("\nAno de Nascimento: ");
+            double ano = double.Parse(Console.ReadLine());
+              
+            Console.Write("Mês de Nascimento: ");
+            double mes = double.Parse(Console.ReadLine());
+              
+            Console.Write("Dia do Nascimento: ");
+            double dias = double.Parse(Console.ReadLine());
+              
+            dias += ( ano * ANO ) + ( mes * MES );
+              
+            Console.WriteLine($"\nSua idade em dias é {dias.ToString(CultureInfo.CreateSpecificCulture("pt-BR"))}\n");
+        }
+    }
+}
